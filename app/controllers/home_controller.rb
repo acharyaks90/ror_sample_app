171e80a5353c69038cd77b37074ce847a03ea170
Acharya Anil Suryavanshi <thakurinfotech@gmail.com>
2020-04-25 11:59:14 +0530

    Adding question

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,11 @@ class HomeController < ApplicationController
   end
   def about
   end
+  def question
+  end
+  def answer
+  end
+  def temp
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  resources :questions
   root 'home#index'
   get 'home/index'
   get '/about' => 'home#about'
+  get '/about/question' => 'home#question'
+  get '/about/answer' => 'home#answer'
+  post '/questions' => 'home#temp'
+  get '/questions/:id' => 'home#questiondetails'
+  post '/questions/answers'=> 'home#temp'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
