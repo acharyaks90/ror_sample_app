d985825db821b76a8d8484d3c4b58eceb6d114ba
Acharya Anil Suryavanshi <thakurinfotech@gmail.com>
2020-04-28 21:45:21 +0530

    Question rendering

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,6 +4,10 @@ class QuestionsController < ApplicationController
         redirect_to root_path
     end
 
+    def show
+        @question = Question.find(params[:id])
+    end
+
     private 
 
     def question_params
