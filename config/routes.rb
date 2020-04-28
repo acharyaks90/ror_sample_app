690f8997790446178b8286851acb409c829fbcf3
Acharya Anil Suryavanshi <thakurinfotech@gmail.com>
2020-04-28 21:24:27 +0530

    Question create working fine

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/about' => 'home#about'
   get '/about/question' => 'home#question'
   get '/about/answer' => 'home#answer'
-  post '/questions' => 'home#temp'
+  # post '/questions' => 'home#temp'
   get '/questions/:id' => 'home#questiondetails'
   post '/questions/answers'=> 'home#temp'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
